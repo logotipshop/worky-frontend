481895d3cb14a8d313ed61f885dd68d2a3397703
logotipshop <logotipshop@gmail.com>
2026-06-17 08:00:10 +0500

    fix: index.js ichida App fayliga .jsx kengaytmasi qo'shildi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx'; // 👈 Oxiriga .jsx (yoki fayling .js bo'lsa .js) qo'shib qo'y!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
